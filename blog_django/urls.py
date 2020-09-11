14862be7d0edce0019a93a89e9f2b0f2cfc18c32
Vitalii Savchuk <savchuk.vitalia@gmail.com>
2020-09-11 13:42:22 +0300

    Authorization and logout

--- blog_django/urls.py
+++ blog_django/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users import views as userViews
+from django.contrib.auth import views as authViews
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('reg/', userViews.register,name = 'reg'),
+    path('user/', authViews.LoginView.as_view(template_name = 'users/user.html'),name = 'auth'),
+    path('exit/', authViews.LogoutView.as_view(template_name = 'users/exit.html'),name = 'exit'),
+    path('profile/', userViews.profile,name = 'profile'),
     path('', include('blog.urls')),
     ]
--- users/views.py
+++ users/views.py
@@ -2,15 +2,22 @@ from django.shortcuts import render,redirect
 # from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserOurRegistration
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+
 def register(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = UserOurRegistration(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'acc {username} was created')
-            return redirect('blog-home')
+            username =form.cleaned_data.get('username')
+            messages.success(request, f'Аккаунт {username} был создан, введите имя пользователя и пароль для авторизации')
+            return redirect('auth')
     else:
         form = UserOurRegistration()
-    return render(request, 'users/registration.html', {'form': form, 'title': 'Регистарция пользователя'})
+    return render(request,'users/registration.html',{'form': form, 'title': 'Регистарция пользователя'})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
